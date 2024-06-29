cd2af748004fb250373dd30838a19037319b8719
denisolid <dkolisnichienko321@gmail.com>
2024-06-29 11:49:51 +0000

    completed

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,3 +1,19 @@
-const FriendList = () => { }
+import FriendListItem from "./FriendsListItem";
+import s from "./FriendList.module.css";
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.friends}>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default FriendList;
